625fb0c7a1c47c738acfe9da5f81f53103a7976f
Perc2val <fsb099@aol.de>
2026-03-13 20:55:57 +0100

    ui done

--- javascript.js
+++ javascript.js
@@ -13,8 +13,15 @@ increment human or computer score
 play game with calling play round 5 times
 */
 
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+const resultScore = document.querySelector(".resultScore");
+const resultMessage = document.querySelector(".resultMessage");
 
 
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
     let computerChoice = "";
@@ -28,67 +35,88 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanInput = prompt("Type in Rock, Paper or Scissor", "")
-    if (humanInput == null || humanInput == undefined){
-        getHumanChoice()
-    }
-    let humanChoice = humanInput.slice(0, 1).toUpperCase() + humanInput.slice(1).toLowerCase();
-    if (!(humanChoice === "Rock" || humanChoice === "Paper" || humanChoice === "Scissor")) {
-        getHumanChoice();
-    } else {
-        return humanChoice;
-    }
-}
-
-
-function playGame(i){
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    
-
-    function playRound(computerChoice, humanChoice){
-        
+function playRound(computerChoice, humanChoice){
         if (computerChoice === humanChoice){
-            console.log("You both chose the same, noone gets a point")
+            displayWinner.textContent = "You both chose the same, noone gets a point";
             return;
         } else if (computerChoice === "Rock" && humanChoice === "Scissor"){
-            console.log("You lose! Rock beats Scissor")
             computerScore++;
             return;
         } else if (computerChoice === "Paper" && humanChoice === "Rock"){
-            console.log("You lose! Paper wrap Rocks")
             computerScore++;
             return;
         } else if (computerChoice === "Scissor" && humanChoice === "Paper"){
-            console.log("You lose! Scissors cuts Paper")
             computerScore++;
             return;
         } else {
-            console.log(`You win! ${humanChoice} wins against ${computerChoice}`)
             humanScore++;
             return;
         }
-    }
-for (let j = 0; j <= i; j++){
-    if (i == j){
-        if (humanScore === computerScore){
-            console.log(`Its a tie! You both have a score of ${humanScore}`);
-            return;
-        } else if(humanScore > computerScore){
-            console.log(`You won the whole game! Score is ${humanScore}: ${computerScore}`);
-            return;
-        } else {
-            console.log(`You lost the whole game! Score is ${humanScore}:${computerScore}`);
-            return;
-        }
-                    
-    } else {
-        const humanSelection = getHumanChoice()
-        const computerSelection = getComputerChoice();
-        playRound(computerSelection, humanSelection)
-        }
-    }
 }
+let displayHumanScore = document.createElement("p");
+let displayComputerScore = document.createElement("p");
+let displayWinner = document.createElement("p");
+
+function checkWinner(){
+    
+    resultMessage.appendChild(displayWinner);
+    if (humanScore === 5){
+        displayWinner.textContent = "You won this first to five";
+    } else if (computerScore === 5){
+        displayWinner.textContent = "You lost this first to five";
+    } return
+}
+
+function displayScore(){
+    displayHumanScore.textContent = `Human Score ${humanScore}`;
+    resultScore.appendChild(displayHumanScore);
+    displayComputerScore.textContent = `Computer Score ${computerScore}`;
+    resultScore.appendChild(displayComputerScore);
+    checkWinner();
+}
+
+rock.addEventListener("click", ()=>  {
+    const computerSelection = getComputerChoice()
+    if (displayWinner.textContent == "" || displayWinner.textContent == "You both chose the same, noone gets a point"){
+        displayWinner = "";
+        playRound(computerSelection, "Rock");
+        displayScore();
+    } else {
+        humanScore = 0;
+        computerScore = 0;
+        displayWinner.textContent = "";
+        playRound(computerSelection, "Rock");
+        displayScore();
+    }
+    
+});
+
+paper.addEventListener("click", ()=>  {
+    const computerSelection = getComputerChoice()
+    if (displayWinner.textContent == "" || displayWinner.textContent == "You both chose the same, noone gets a point"){
+        displayWinner.textContent = "";
+        playRound(computerSelection, "Paper");
+        displayScore();
+    } else {
+        humanScore = 0;
+        computerScore = 0;
+        displayWinner.textContent = "";
+        playRound(computerSelection, "Paper");
+        displayScore();
+    }
+});
+
+scissor.addEventListener("click", ()=>  {
+    const computerSelection = getComputerChoice()
+    if (displayWinner.textContent == "" || displayWinner.textContent == "You both chose the same, noone gets a point"){
+        displayWinner.textContent = "";
+        playRound(computerSelection, "Scissor");
+        displayScore();
+    } else {
+        humanScore = 0;
+        computerScore = 0;
+        displayWinner.textContent = "";
+        playRound(computerSelection, "Scissor");
+        displayScore();
+    }
+});
